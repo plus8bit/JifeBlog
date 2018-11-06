417cc6706ab98b287e29c303cc7f9f2fc58fd12a
Nick <plus0bit@gmail.com>
2018-11-07 01:33:07 +0300

    Init database, make @db

--- app.rb
+++ app.rb
@@ -2,6 +2,16 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sqlite3'
+
+def init_db
+	@db = SQLite3::Database.new 'jifeblog.db'
+	@db.results_as_hash = true
+end
+
+before do
+	init_db
+end
 
 get '/' do
 	erb "Hello!"		
