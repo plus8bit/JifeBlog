b2bba2b56679e55b04b9246ef59147b5d89fd4b7
Nick <plus0bit@gmail.com>
2018-11-07 18:24:48 +0300

    Display comments for post

--- app.rb
+++ app.rb
@@ -59,6 +59,9 @@ get '/details/:post_id' do
   results = @db.execute 'select * from Posts where id = ?', [post_id]
   # choose this one post in variable @row
   @row = results[0]
+
+  # Choice comment for post
+  @comments = @db.execute 'select * from Comments where post_id = ? order by id', [post_id]
 	
   # return view details.erb
   erb :details
