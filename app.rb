c9ab844fcfee4f236d6e2c381132c6b90f1f7ef8
Nick <plus0bit@gmail.com>
2018-11-07 12:52:35 +0300

    Add comments

--- app.rb
+++ app.rb
@@ -9,12 +9,16 @@ def init_db
 	@db.results_as_hash = true
 end
 
+# calls "before" every time when page reload
 before do
+	
 	init_db
 end
 
 configure do
+	# Initialization bd
 	init_db
+	# Create bd if table not exists
 	@db.execute 'CREATE TABLE IF NOT EXISTS Posts (id INTEGER PRIMARY KEY AUTOINCREMENT, created_date DATE, content TEXT);'
 end
 
@@ -28,5 +32,6 @@ end
 
 post '/new' do
 	content = params[:content]
+
 	erb "You typer #{content}"
   end
